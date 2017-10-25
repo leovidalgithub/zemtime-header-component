f451b775a1e5673cc88912136c4ba60eba983077
Leonardo <leo@leovidal.es>
2017-10-25 10:26:16 +0200

    25-oct-2017

--- src/ia-comp-header.component.ts
+++ src/ia-comp-header.component.ts
@@ -1,15 +1,16 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'ia-comp-header',
   templateUrl: './ia-comp-header.component.html',
   styleUrls: ['./ia-comp-header.component.scss']
 })
 export class IaCompHeaderComponent implements OnInit {
 
-  @Input() compHeaderSectionData: object;
-  @Input() compHeaderActionsShow: object;
-  @Input() compHeaderButtonsActions: Array<object>;
+  @Input() compHeaderSectionData: Object;
+  @Input() compHeaderActionsShow: Object;
+  @Input() compHeaderButtonsActions: Array<Object>;
 
   constructor() { }
 
